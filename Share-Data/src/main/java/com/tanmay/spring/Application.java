a7f5900479461760d55ce0cfb14387b7d6588e04
modhtanmay <modhtanmay@gmail.com>
2020-08-15 13:44:06 +0530

    Second Commit

--- Share-Data/src/main/java/com/tanmay/spring/Application.java
+++ Share-Data/src/main/java/com/tanmay/spring/Application.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);//launches a spring boot Application
+		SpringApplication.run(Application.class, args);///launches a spring boot Application
 	}
 
 }
